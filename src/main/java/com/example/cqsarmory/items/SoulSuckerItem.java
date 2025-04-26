9f3b01f4676ee0db9b90fa3e1359fe5a08b20a01
cqpog <cforst80@gmail.com>
2025-04-26 02:04:16 -0500

    Update SoulSuckerItem.java

--- src/main/java/com/example/cqsarmory/items/SoulSuckerItem.java
+++ src/main/java/com/example/cqsarmory/items/SoulSuckerItem.java
@@ -1,6 +1,16 @@
 package com.example.cqsarmory.items;
 
 import io.redspace.ironsspellbooks.api.util.Utils;
+import io.redspace.ironsspellbooks.capabilities.magic.MagicManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.damagesource.DamageSource;
@@ -9,8 +19,10 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.WindChargeItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.HitResult;
+import net.neoforged.neoforge.common.data.SoundDefinition;
 
 public class SoulSuckerItem extends Item {
 
@@ -24,14 +36,22 @@ public class SoulSuckerItem extends Item {
         var entities = level.getEntities(livingEntity, livingEntity.getBoundingBox().expandTowards(proj), entity -> {
             return Utils.checkEntityIntersecting(entity, livingEntity.getEyePosition(), (livingEntity.getEyePosition().add(proj)), 0.15f).getType() == HitResult.Type.ENTITY;
         });
-//        if (remainingUseDuration % 20 == 0) {
-//            MagicManager.spawnParticles(livingEntity.level(), ParticleTypes.EXPLOSION, ((livingEntity.getForward().x()) * 2) + livingEntity.getEyePosition().x(), ((livingEntity.getForward().y()) * 2) + livingEntity.getEyePosition().y(), ((livingEntity.getForward().z()) * 2) + livingEntity.getEyePosition().z(), 1, 0, 0, 0, 0, false);
-//        }
+        if (remainingUseDuration % 5 == 0) {
+            if (livingEntity instanceof ServerPlayer serverplayer) {
+                ServerLevel serverlevel = (ServerLevel) livingEntity.level();
+                serverlevel.playSound(
+                        null, serverplayer.getX(), serverplayer.getY(), serverplayer.getZ(), SoundEvents.WIND_CHARGE_BURST, serverplayer.getSoundSource(), 2F, 1.5F
+                );
+                MagicManager.spawnParticles(serverlevel, ParticleTypes.POOF, ((livingEntity.getForward().x()) * 5) + livingEntity.getEyePosition().x(), ((livingEntity.getForward().y()) * 5) + livingEntity.getEyePosition().y(), ((livingEntity.getForward().z()) * 5) + livingEntity.getEyePosition().z(), 3, 0, 0, 0, 1, false);
+            }        }
         if (entities.size() > 0) {
             for (Entity Bob : entities) {
                 if (remainingUseDuration % 5 == 0) {
                     Bob.push(livingEntity.getLookAngle().scale(-0.25).subtract(0, 0, 0));
                     Bob.hurt(genericDamage, 10.0f);
+
+
+
                 }
             }
         }
