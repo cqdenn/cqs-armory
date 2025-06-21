761eb0a7cc93537ea53630c1632c3d15549ff525
cqpog <cforst80@gmail.com>
2025-06-20 22:11:54 -0500

    shield system

--- src/main/java/com/example/cqsarmory/data/AbilityData.java
+++ src/main/java/com/example/cqsarmory/data/AbilityData.java
@@ -2,10 +2,12 @@ package com.example.cqsarmory.data;
 
 import com.example.cqsarmory.registry.EntityDataAttachmentRegistry;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ShieldItem;
 
 public class AbilityData {
     public Mjolnir mjolnirData = new Mjolnir();
     public CosmicArk cosmicArk = new CosmicArk();
+    public float currentShieldDamage;
 
     public class Mjolnir {
         public boolean doDamage;
