bf18ef93140af4d390269756baf7acb9f1958f16
cqpog <cforst80@gmail.com>
2025-06-20 22:11:39 -0500

    balances

--- src/main/java/com/example/cqsarmory/registry/ExtendedWeaponTier.java
+++ src/main/java/com/example/cqsarmory/registry/ExtendedWeaponTier.java
@@ -91,11 +91,11 @@ public class ExtendedWeaponTier implements Tier {
         IRON = new ExtendedWeaponTier("iron", 1, 250, 2.0F, 0.0F, 4, BlockTags.INCORRECT_FOR_IRON_TOOL, () -> Ingredient.of(new ItemLike[]{Items.IRON_INGOT}));
         GOLD = new ExtendedWeaponTier("gold", 1.2f, 150, 1.0F, 0.0F, 5, BlockTags.INCORRECT_FOR_GOLD_TOOL, () -> Ingredient.of(new ItemLike[]{Items.GOLD_INGOT}));
         DIAMOND = new ExtendedWeaponTier("diamond", 1.5F, 250, 3.0F, 0.2F, 4, BlockTags.INCORRECT_FOR_DIAMOND_TOOL, () -> Ingredient.of(new ItemLike[]{Items.DIAMOND}));
-        NETHERITE = new ExtendedWeaponTier("netherite", 2, 2000, 4.0F, 0.0F, 4, BlockTags.INCORRECT_FOR_NETHERITE_TOOL, () -> Ingredient.of(new ItemLike[]{Items.NETHERITE_INGOT}));
+        NETHERITE = new ExtendedWeaponTier("netherite", 4, 2000, 4.0F, 0.0F, 4, BlockTags.INCORRECT_FOR_NETHERITE_TOOL, () -> Ingredient.of(new ItemLike[]{Items.NETHERITE_INGOT}));
         SCULK = new ExtendedWeaponTier("sculk", 2, 2000, 2.0F, 0.2F, 4, BlockTags.INCORRECT_FOR_DIAMOND_TOOL, () -> Ingredient.of(new ItemLike[]{Items.SCULK}));
         OBSIDIAN = new ExtendedWeaponTier("obsidian", 1, 3000, 4.0F, -0.3F, 4, BlockTags.INCORRECT_FOR_IRON_TOOL, () -> Ingredient.of(new ItemLike[]{Items.OBSIDIAN}));
         AMETHYST = new ExtendedWeaponTier("amethyst", 1, 300, 1.0F, 0.2F, 4, BlockTags.INCORRECT_FOR_STONE_TOOL, () -> Ingredient.of(new ItemLike[]{Items.AMETHYST_SHARD}));
-        BLAZING = new ExtendedWeaponTier("blazing", 6, 2500, 2.0F, 0.4F, 4, BlockTags.INCORRECT_FOR_NETHERITE_TOOL, () -> Ingredient.of(new ItemLike[]{ItemRegistry.BLAZING_WEAPONSET.ingot().get()}));
+        BLAZING = new ExtendedWeaponTier("blazing", 5, 2500, 2.0F, 0.4F, 4, BlockTags.INCORRECT_FOR_NETHERITE_TOOL, () -> Ingredient.of(new ItemLike[]{ItemRegistry.BLAZING_WEAPONSET.ingot().get()}));
         LIVING = new ExtendedWeaponTier("living", 3, 2000, 5.0F, -0.1F, 4, BlockTags.INCORRECT_FOR_NETHERITE_TOOL, () -> Ingredient.of(new ItemLike[]{ItemRegistry.LIVING_WEAPONSET.ingot().get()}));
         UMBRITE = new ExtendedWeaponTier("umbrite", 3, 2000, 1.0F, 0.5F, 4, BlockTags.INCORRECT_FOR_NETHERITE_TOOL, () -> Ingredient.of(new ItemLike[]{ItemRegistry.UMBRITE_WEAPONSET.ingot().get()}));
         SILVERSTEEL = new ExtendedWeaponTier("silversteel", 3, 2000, 0.0F, 0.4F, 4, BlockTags.INCORRECT_FOR_NETHERITE_TOOL, () -> Ingredient.of(new ItemLike[]{ItemRegistry.SILVERSTEEL_WEAPONSET.ingot().get()}));
