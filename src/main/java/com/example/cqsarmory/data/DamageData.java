dd5c99026598cc1763ddb9aebf55b0d00a97fe29
cq <cforst80@gmail.com>
2025-07-21 23:55:30 -0500

    damage data init

---
+++ src/main/java/com/example/cqsarmory/data/DamageData.java
@@ -0,0 +1,15 @@
+package com.example.cqsarmory.data;
+
+import com.example.cqsarmory.registry.EntityDataAttachmentRegistry;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+
+public class DamageData {
+    public DamageSource lastSource;
+    public float lastDamage;
+
+    public static DamageData get (Entity entity) {
+        return entity.getData(EntityDataAttachmentRegistry.DAMAGE_DATA);
+    }
+
+}
