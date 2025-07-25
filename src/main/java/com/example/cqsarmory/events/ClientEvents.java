324c21f20562162e96ea8d7f2c1c4d4ce8c1bb19
cq <cforst80@gmail.com>
2025-07-24 22:12:17 -0500

    reorganize

--- src/main/java/com/example/cqsarmory/events/ClientEvents.java
+++ src/main/java/com/example/cqsarmory/events/ClientEvents.java
@@ -1,47 +1,21 @@
 package com.example.cqsarmory.events;
 
 import com.example.cqsarmory.CqsArmory;
-import com.example.cqsarmory.data.AbilityData;
-import com.example.cqsarmory.items.CosmicArkItem;
-import com.example.cqsarmory.items.VolcanoSwordItem;
 import com.example.cqsarmory.network.StartSuckingPacket;
-import com.example.cqsarmory.registry.AttributeRegistry;
-import com.example.cqsarmory.registry.ItemRegistry;
-import io.redspace.ironsspellbooks.util.ParticleHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageSources;
-import net.minecraft.world.damagesource.DamageType;
-import net.minecraft.world.damagesource.DamageTypes;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Pose;
-import net.minecraft.world.entity.ai.attributes.AttributeInstance;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.level.Level;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.MovementInputUpdateEvent;
-import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
-import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
-import net.neoforged.neoforge.event.entity.living.LivingEquipmentChangeEvent;
-import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
-
-import java.util.Random;
 
 
 @EventBusSubscriber(Dist.CLIENT)
