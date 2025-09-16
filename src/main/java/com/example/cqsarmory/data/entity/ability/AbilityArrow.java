9252327ae68366902d8b1341b65fc796cec736eb
cqpog <cforst80@gmail.com>
2025-09-15 21:55:46 -0500

    ability arrow now supports potion effects

--- src/main/java/com/example/cqsarmory/data/entity/ability/AbilityArrow.java
+++ src/main/java/com/example/cqsarmory/data/entity/ability/AbilityArrow.java
@@ -1,15 +1,20 @@
 package com.example.cqsarmory.data.entity.ability;
 
 import com.example.cqsarmory.registry.EntityRegistry;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
@@ -60,6 +65,43 @@ public class AbilityArrow extends AbstractArrow {
     @Override
     protected boolean tryPickup(Player player) {
         return false;
+    }
+
+    private PotionContents getPotionContents() {
+        return this.getPickupItemStackOrigin().getOrDefault(DataComponents.POTION_CONTENTS, PotionContents.EMPTY);
+    }
+
+    private void setPotionContents(PotionContents potionContents) {
+        this.getPickupItemStackOrigin().set(DataComponents.POTION_CONTENTS, potionContents);
+    }
+
+    public void addEffect(MobEffectInstance effectInstance) {
+        this.setPotionContents(this.getPotionContents().withEffectAdded(effectInstance));
+    }
+
+    @Override
+    protected void doPostHurtEffects(LivingEntity living) {
+        super.doPostHurtEffects(living);
+        Entity entity = this.getEffectSource();
+        PotionContents potioncontents = this.getPotionContents();
+        if (potioncontents.potion().isPresent()) {
+            for (MobEffectInstance mobeffectinstance : potioncontents.potion().get().value().getEffects()) {
+                living.addEffect(
+                        new MobEffectInstance(
+                                mobeffectinstance.getEffect(),
+                                Math.max(mobeffectinstance.mapDuration(p_268168_ -> p_268168_ / 8), 1),
+                                mobeffectinstance.getAmplifier(),
+                                mobeffectinstance.isAmbient(),
+                                mobeffectinstance.isVisible()
+                        ),
+                        entity
+                );
+            }
+        }
+
+        for (MobEffectInstance mobeffectinstance1 : potioncontents.customEffects()) {
+            living.addEffect(mobeffectinstance1, entity);
+        }
     }
 
     @Override
