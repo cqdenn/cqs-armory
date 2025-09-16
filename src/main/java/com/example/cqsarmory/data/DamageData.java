9558e212b59b7c05efabd370ea6607090bf0a21c
cqpog <cforst80@gmail.com>
2025-09-15 21:56:16 -0500

    hunters mark

--- src/main/java/com/example/cqsarmory/data/DamageData.java
+++ src/main/java/com/example/cqsarmory/data/DamageData.java
@@ -3,10 +3,12 @@ package com.example.cqsarmory.data;
 import com.example.cqsarmory.registry.EntityDataAttachmentRegistry;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 
 public class DamageData {
     public DamageSource lastSource;
     public float lastDamage;
+    public LivingEntity markedBy;
 
     public static DamageData get (Entity entity) {
         return entity.getData(EntityDataAttachmentRegistry.DAMAGE_DATA);
