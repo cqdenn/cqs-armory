6592eeeac85cd2ecdc4055f14e5fa03286783ac9
cqpog <cforst80@gmail.com>
2025-09-15 19:43:17 -0500

    firework quiver

---
+++ src/main/java/com/example/cqsarmory/data/entity/ability/FireworkProjectile.java
@@ -0,0 +1,124 @@
+package com.example.cqsarmory.data.entity.ability;
+
+import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
+import io.redspace.ironsspellbooks.api.util.Utils;
+import io.redspace.ironsspellbooks.damage.DamageSources;
+import it.unimi.dsi.fastutil.ints.IntImmutableList;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MoverType;
+import net.minecraft.world.entity.projectile.FireworkRocketEntity;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.FireworkExplosion;
+import net.minecraft.world.item.component.Fireworks;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.*;
+
+import java.util.List;
+import java.util.Random;
+
+public class FireworkProjectile extends FireworkRocketEntity {
+    public FireworkProjectile(Level level, ItemStack stack, Entity shooter, double x, double y, double z, boolean shotAtAngle, float damage) {
+        super(level, stack, shooter, x, y, z, shotAtAngle);
+        this.damage = damage;
+        this.life = 0;
+        this.lifetime = 60;
+    }
+
+    private final float damage;
+    private int life;
+    private int lifetime;
+
+    public float getDamage() {
+        return damage;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (!this.isShotAtAngle()) {
+            double d2 = this.horizontalCollision ? 1.0 : 1.15;
+            this.setDeltaMovement(this.getDeltaMovement().multiply(d2, 1.0, d2).add(0.0, 0.04, 0.0));
+        }
+
+        Vec3 vec33 = this.getDeltaMovement();
+        this.move(MoverType.SELF, vec33);
+        this.setDeltaMovement(vec33);
+
+
+        HitResult hitresult = ProjectileUtil.getHitResultOnMoveVector(this, this::canHitEntity);
+        if (!this.noPhysics) {
+            this.hitTargetOrDeflectSelf(hitresult);
+            this.hasImpulse = true;
+        }
+
+        this.updateRotation();
+        if (this.life == 0 && !this.isSilent()) {
+            this.level().playSound(null, this.getX(), this.getY(), this.getZ(), SoundEvents.FIREWORK_ROCKET_LAUNCH, SoundSource.AMBIENT, 3.0F, 1.0F);
+        }
+
+        this.life++;
+        if (this.level().isClientSide && this.life % 2 < 2) {
+            this.level()
+                    .addParticle(
+                            ParticleTypes.FIREWORK,
+                            this.getX(),
+                            this.getY(),
+                            this.getZ(),
+                            this.random.nextGaussian() * 0.05,
+                            -this.getDeltaMovement().y * 0.5,
+                            this.random.nextGaussian() * 0.05
+                    );
+        }
+
+        if (!this.level().isClientSide && this.life > this.lifetime) {
+            this.explode();
+        }
+    }
+
+    @Override
+    protected void onHitEntity(EntityHitResult result) {
+        super.onHitEntity(result);
+        if (!this.level().isClientSide) {
+            this.explode();
+        }
+    }
+
+    @Override
+    protected void onHitBlock(BlockHitResult result) {
+        BlockPos blockpos = new BlockPos(result.getBlockPos());
+        this.level().getBlockState(blockpos).entityInside(this.level(), blockpos, this);
+        if (!this.level().isClientSide()) {
+            this.explode();
+        }
+
+        super.onHitBlock(result);
+    }
+
+    private void explode() {
+        this.level().broadcastEntityEvent(this, (byte) 17);
+        this.gameEvent(GameEvent.EXPLODE, this.getOwner());
+        this.dealExplosionDamage();
+        this.discard();
+    }
+
+    private void dealExplosionDamage() {
+        Vec3 hitPos = this.position();
+        Level level = this.level();
+        double explosionRadius = 2;
+        for (LivingEntity livingentity : level.getEntitiesOfClass(LivingEntity.class, new AABB(hitPos.subtract(explosionRadius, explosionRadius, explosionRadius), hitPos.add(explosionRadius, explosionRadius, explosionRadius)))) {
+            if (livingentity.isAlive() && livingentity.isPickable() && Utils.hasLineOfSight(level, hitPos, livingentity.getBoundingBox().getCenter(), true)) {
+                DamageSources.applyDamage(livingentity, this.getDamage(), damageSources().fireworks(this, this.getOwner()));
+            }
+        }
+    }
+}
--- src/main/java/com/example/cqsarmory/items/curios/quivers/FireworkQuiver.java
+++ src/main/java/com/example/cqsarmory/items/curios/quivers/FireworkQuiver.java
@@ -1,12 +1,23 @@
 package com.example.cqsarmory.items.curios.quivers;
 
+import com.example.cqsarmory.data.entity.ability.FireworkProjectile;
 import com.example.cqsarmory.items.curios.QuiverItem;
+import io.redspace.bowattributes.registry.BowAttributes;
 import io.redspace.ironsspellbooks.api.registry.SpellDataRegistryHolder;
+import io.redspace.ironsspellbooks.entity.spells.ExtendedFireworkRocket;
+import it.unimi.dsi.fastutil.ints.IntImmutableList;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.FireworkRocketEntity;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.FireworkExplosion;
+import net.minecraft.world.item.component.Fireworks;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.List;
+import java.util.Random;
 
 public class FireworkQuiver extends QuiverItem {
     public FireworkQuiver(Properties properties, SpellDataRegistryHolder[] spellDataRegistryHolders) {
@@ -15,7 +26,51 @@ public class FireworkQuiver extends QuiverItem {
 
     @Override
     public Projectile getCustomProjectile(Projectile arrow, Player shooter, ItemStack projectileStack, ItemStack weaponStack) {
-        ItemStack fireworkStack = new ItemStack(Items.FIREWORK_ROCKET);
-        return new FireworkRocketEntity(shooter.level(), fireworkStack, shooter, shooter.getX(), shooter.getEyeY() - 0.15F, shooter.getZ(), true);
+        return new FireworkProjectile(shooter.level(), randomFireworkRocket(), shooter, shooter.getX(), shooter.getEyeY() - 0.15F, shooter.getZ(), true, (float) shooter.getAttribute(BowAttributes.ARROW_DAMAGE).getValue());
     }
+
+    //praise Iron431
+    private ItemStack randomFireworkRocket() {
+        Random random = new Random();
+        ItemStack rocket = new ItemStack(Items.FIREWORK_ROCKET);
+        FireworkExplosion.Shape shape;
+        byte type = (byte) (random.nextInt(3) * 2);
+        if (random.nextFloat() < .08f) { //8% chance for creeper face explosion
+            type = 3;
+        }
+        shape = FireworkExplosion.Shape.values()[type];
+        var fireworks = new Fireworks(-1, List.of(new FireworkExplosion(shape, new IntImmutableList(randomColors()), new IntImmutableList(new int[0]), random.nextInt(3) == 0, random.nextInt(3) == 0)));
+        rocket.set(DataComponents.FIREWORKS, fireworks);
+        return rocket;
+    }
+
+    private int[] randomColors() {
+        int[] colors = new int[3];
+        Random random = new Random();
+
+        for (int i = 0; i < colors.length; i++) {
+            colors[i] = DYE_COLORS[random.nextInt(DYE_COLORS.length)];
+        }
+        return colors;
+    }
+
+    //https://minecraft.fandom.com/wiki/Dye#Color_values
+    private static final int[] DYE_COLORS = {
+            //1908001,
+            11546150,
+            6192150,
+            //8606770,
+            3949738,
+            8991416,
+            1481884,
+            //10329495,
+            //4673362,
+            15961002,
+            8439583,
+            16701501,
+            3847130,
+            13061821,
+            16351261,
+            16383998
+    };
 }
