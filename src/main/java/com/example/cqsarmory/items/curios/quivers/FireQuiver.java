d0fbc94bd5f1a040405909560558a28280f5f41b
cqpog <cforst80@gmail.com>
2025-09-23 18:28:20 -0500

    clearer descriptions

--- src/main/java/com/example/cqsarmory/items/curios/quivers/FireQuiver.java
+++ src/main/java/com/example/cqsarmory/items/curios/quivers/FireQuiver.java
@@ -4,12 +4,26 @@ import com.example.cqsarmory.data.entity.ability.AbilityArrow;
 import com.example.cqsarmory.data.entity.ability.FireArrow;
 import com.example.cqsarmory.items.curios.QuiverItem;
 import io.redspace.ironsspellbooks.api.registry.SpellDataRegistryHolder;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class FireQuiver extends QuiverItem {
-    public FireQuiver(Properties properties, SpellDataRegistryHolder[] spellDataRegistryHolders) {
+    final @Nullable String slotIdentifier;
+    Style descriptionStyle;
+    boolean showHeader;
+
+    public FireQuiver(Properties properties, String slotIdentifier, SpellDataRegistryHolder[] spellDataRegistryHolders) {
         super(properties, spellDataRegistryHolders);
+        this.slotIdentifier = slotIdentifier;
+        this.showHeader = true;
+        descriptionStyle = Style.EMPTY.withColor(ChatFormatting.YELLOW);
     }
 
     @Override
@@ -17,5 +31,27 @@ public class FireQuiver extends QuiverItem {
         FireArrow abilityArrow = new FireArrow(shooter.level());
         abilityArrow.copyStats(arrow, shooter, arrowDmg);
         return abilityArrow;
+    }
+
+    @Override
+    public List<Component> getAttributesTooltip(List<Component> tooltips, TooltipContext tooltipContext, ItemStack stack) {
+        var attrTooltip = super.getAttributesTooltip(tooltips, tooltipContext, stack);
+        boolean needHeader = attrTooltip.isEmpty();
+        var descriptionLines = getDescriptionLines(stack);
+        if (needHeader && !descriptionLines.isEmpty()) {
+            attrTooltip.add(Component.empty());
+            attrTooltip.add(Component.translatable("curios.modifiers." + slotIdentifier).withStyle(ChatFormatting.GOLD));
+        }
+        attrTooltip.addAll(descriptionLines);
+
+        return attrTooltip;
+    }
+
+    public List<Component> getDescriptionLines(ItemStack stack) {
+        return List.of(getDescription(stack));
+    }
+
+    public Component getDescription(ItemStack stack) {
+        return Component.literal(" ").append(Component.translatable(this.getDescriptionId() + ".desc")).withStyle(descriptionStyle);
     }
 }
