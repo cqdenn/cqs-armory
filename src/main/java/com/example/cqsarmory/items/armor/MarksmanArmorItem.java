558d3c54a088c8cc30b2c39c28edd0cc22012c0a
cqpog <cforst80@gmail.com>
2025-09-25 13:00:45 -0500

    nerfed

--- src/main/java/com/example/cqsarmory/items/armor/MarksmanArmorItem.java
+++ src/main/java/com/example/cqsarmory/items/armor/MarksmanArmorItem.java
@@ -6,10 +6,10 @@ import io.redspace.ironsspellbooks.item.weapons.AttributeContainer;
 
 public class MarksmanArmorItem extends ExtendedArmorItem {
     public MarksmanArmorItem(Type type, Properties properties, AttributeContainer extraAttribute) {
-        super(ArmorMaterialsRegistry.MARKSMAN, type, properties, genericArcherArmorAttributes(0.25f, 0f, 1, 0.02f, 0, extraAttribute));
+        super(ArmorMaterialsRegistry.MARKSMAN, type, properties, genericArcherArmorAttributes(0.2f, 0f, 1, 0.02f, 0, extraAttribute));
     }
     public MarksmanArmorItem(Type type, Properties properties) {
-        super(ArmorMaterialsRegistry.MARKSMAN, type, properties, genericArcherArmorAttributes(0.25f, 0f, 1, 0.02f, 0));
+        super(ArmorMaterialsRegistry.MARKSMAN, type, properties, genericArcherArmorAttributes(0.2f, 0f, 1, 0.02f, 0));
     }
 
 
