caf8ceaeafd86bfd9114767988e9d1e237d85d09
cqpog <cforst80@gmail.com>
2026-02-18 22:03:42 -0600

    gecko chains

--- src/main/java/com/example/cqsarmory/events/ClientEvents.java
+++ src/main/java/com/example/cqsarmory/events/ClientEvents.java
@@ -19,6 +19,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -31,10 +32,14 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.earlydisplay.ElementShader;
 import net.neoforged.neoforge.client.event.*;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.entity.PartEntity;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 import org.joml.Quaternionf;
 import org.joml.Vector3f;
+import software.bernie.geckolib.event.GeckoLibEventsNeoForge;
+import software.bernie.geckolib.event.GeoRenderEvent;
+import software.bernie.geckolib.service.GeckoLibEvents;
 
 import javax.swing.plaf.basic.BasicTreeUI;
 
@@ -115,6 +120,32 @@ public class ClientEvents {
                     Vec3 translate = dir.scale(living.getHitbox().getXsize() / 2).scale(0.5);
                     poseStack.translate(translate.x, translate.y, translate.z);
                     RenderingUtils.renderChainBetween(start, to.add(dir.scale(1)), event.getPoseStack(), event.getMultiBufferSource());
+                    poseStack.popPose();
+                }
+            }
+            poseStack.popPose();
+        }
+    }
+
+    @SubscribeEvent
+    public static void chainedGecko (GeoRenderEvent.Entity.Post event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof LivingEntity living && living.hasEffect(MobEffectRegistry.CHAINED) && DamageData.get(living).chainWhipLocation != null) {
+            float chainHeight = 1;
+            Vec3 old = new Vec3(living.xo, living.yo, living.zo);
+            Vec3 start = old.add(living.position().subtract(old).scale(event.getPartialTick()));
+            Vec3 to = DamageData.get(living).chainWhipLocation.subtract(0, chainHeight, 0);
+            PoseStack poseStack = event.getPoseStack();
+            poseStack.pushPose();
+            poseStack.translate(0, chainHeight, 0);
+
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    poseStack.pushPose();
+                    Vec3 dir = new Vec3((i - 0.5) * j, 0, (i - 0.5) * (1 - j)).scale(2);
+                    Vec3 translate = dir.scale(living.getHitbox().getXsize() / 2).scale(0.5);
+                    poseStack.translate(translate.x, translate.y, translate.z);
+                    RenderingUtils.renderChainBetween(start, to.add(dir.scale(1)), event.getPoseStack(), event.getBufferSource());
                     poseStack.popPose();
                 }
             }
