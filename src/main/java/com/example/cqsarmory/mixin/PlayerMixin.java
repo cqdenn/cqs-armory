8ad280f9f83579b42fddb89b87818e094d7630b8
cqpog <cforst80@gmail.com>
2026-03-04 21:09:49 -0600

    slipstream

--- src/main/java/com/example/cqsarmory/mixin/PlayerMixin.java
+++ src/main/java/com/example/cqsarmory/mixin/PlayerMixin.java
@@ -1,9 +1,13 @@
 package com.example.cqsarmory.mixin;
 
 import com.example.cqsarmory.data.AbilityData;
+import com.example.cqsarmory.registry.ItemRegistry;
 import com.example.cqsarmory.utils.CQtils;
+import io.redspace.ironsspellbooks.api.util.Utils;
 import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Unit;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffects;
@@ -11,6 +15,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -19,6 +24,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin {
+
+    @Shadow public abstract void playNotifySound(SoundEvent sound, SoundSource source, float volume, float pitch);
+
+    @Inject(method = "getFlyingSpeed", at = @At("HEAD"), cancellable = true)
+    private void cqs_armory$overrideAirStrafing(CallbackInfoReturnable<Float> cir) {
+        Player player = (Player) (Object) this;
+        if (player.getAbilities().flying && !player.isPassenger()) {
+            cir.setReturnValue(player.isSprinting() ? player.getAbilities().getFlyingSpeed() * 2.0F : player.getAbilities().getFlyingSpeed());
+        } else if (ItemRegistry.SLIPSTREAM.get().isEquippedBy(player) && player.getY() >= Utils.findRelativeGroundLevel(player.level(), player.position(), 4) + 3) {//3 or more blocks off ground
+            cir.setReturnValue(player.isSprinting() ? 0.1f : 0.08f);
+        } else {
+            cir.setReturnValue(player.isSprinting() ? 0.025999999F : 0.02F);
+        }
+    }
 
     @Inject(method = "disableShield", at = @At("HEAD"), cancellable = true)
     private void cqs_armory$cancelShield(CallbackInfo ci) {
