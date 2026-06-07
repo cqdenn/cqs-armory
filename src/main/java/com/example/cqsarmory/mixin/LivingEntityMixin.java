24bdf0b9204b4d492c8754f6f98d5a577011e93f
cqpog <cforst80@gmail.com>
2026-06-07 14:42:51 -0500

    shield block delay removed

---
+++ src/main/java/com/example/cqsarmory/mixin/LivingEntityMixin.java
@@ -0,0 +1,22 @@
+package com.example.cqsarmory.mixin;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+
+    @Inject(method = "isBlocking", at = @At("HEAD"), cancellable = true)
+    private void cqs_armory$shieldBlockDelayRemoved(CallbackInfoReturnable<Boolean> cir) {
+        LivingEntity living = (LivingEntity) (Object) this;
+        if (living.isUsingItem() && !living.getUseItem().isEmpty()) {
+            cir.setReturnValue(living.getUseItem().canPerformAction(net.neoforged.neoforge.common.ItemAbilities.SHIELD_BLOCK));
+        } else {
+            cir.setReturnValue(false);
+        }
+    }
+}
